32391caa6c29c88d956ca146b1ccf1a60f216e14
antonnnnnnnnnnnnnnnnnnn <150503680+antonnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-01-31 17:45:32 +1000

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,19 @@
+class ClassName{}
+class Point{x:number; y:number}
+let point: Point = new Point();
+
+function ri(){
+    point.x = 1;
+    point.y = 2;
+}
+ri();
+
+class Point2{width:number; height:number}
+let point20: Point2 = new Point2;
+
+function ri2(){
+    point20.width = 3;
+    point20.height = 5;
+}
+
+ri2()
